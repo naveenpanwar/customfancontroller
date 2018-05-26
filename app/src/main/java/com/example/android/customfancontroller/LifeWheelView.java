78b0be0651e96d8d9a4fe3616d31e6c1c811fb9c
Naveen Panwar <naveen.panwar27@gmail.com>
2018-05-26 20:05:45 +0530

    created arcs to circles

--- app/src/main/java/com/example/android/customfancontroller/LifeWheelView.java
+++ app/src/main/java/com/example/android/customfancontroller/LifeWheelView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -16,6 +17,10 @@ public class LifeWheelView extends View {
     private Paint mCutLinesPaint;
     private Paint mDialPaint;
     private float mRadius;
+    private RectF rect;
+    private Paint paints[] = new Paint[8];
+
+    //float radiusStep = mRadius/5;
 
     private void init() {
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -29,6 +34,23 @@ public class LifeWheelView extends View {
 
         mCutLinesPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mCutLinesPaint.setColor(Color.BLACK);
+
+        paints[0] = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paints[0].setColor(Color.BLACK);
+        paints[1] = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paints[1].setColor(Color.BLUE);
+        paints[2] = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paints[2].setColor(Color.GREEN);
+        paints[3] = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paints[3].setColor(Color.YELLOW);
+        paints[4] = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paints[4].setColor(Color.DKGRAY);
+        paints[5] = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paints[5].setColor(Color.RED);
+        paints[6] = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paints[6].setColor(Color.CYAN);
+        paints[7] = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paints[7].setColor(Color.MAGENTA);
     }
 
     public LifeWheelView(Context context) {
@@ -52,16 +74,30 @@ public class LifeWheelView extends View {
         mWidth = w;
         mHeight = h;
         mRadius = (float) (Math.min(mWidth, mHeight) / 2 * 0.8);
+        rect = new RectF(mWidth/2 - mRadius,mHeight/2 - mRadius,mWidth/2+mRadius,mHeight/2+mRadius);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawCircle(mWidth/2, mHeight/2, mRadius, mDialPaint);
-        canvas.drawLine(mWidth/2,0f,mWidth/2, mHeight, mCutLinesPaint);
-        canvas.drawLine(mWidth,0f,mWidth, mHeight, mCutLinesPaint);
-        canvas.drawLine(0f,mHeight/2,mWidth, mHeight/2, mCutLinesPaint);
-        canvas.drawLine(mWidth/2,0f,mWidth/2, mHeight, mCutLinesPaint);
+        //int paintvar = 0;
+
+        /*
+        for( float i=mRadius; i>=radiusStep; i-=radiusStep) {
+            canvas.drawCircle(mWidth / 2, mHeight / 2, i, paints[paintvar++]);
+        }
+        */
+        for( int i=0; i < 8; i++) {
+            canvas.drawArc(rect, (float) 45 * i,45, true, paints[i]);
+        }
+        canvas.drawLine(mWidth/2,0f,mWidth/2, mHeight, mCutLinesPaint); // vertical bisector
+        canvas.drawLine(0f,mHeight/2,mWidth, mHeight/2, mCutLinesPaint);// horizontal bisector
+
+
+        canvas.drawLine(mWidth/2,mHeight/2,mWidth/2 - mRadius, mHeight/2 - mRadius, mCutLinesPaint);               // right diagonal
+        canvas.drawLine(mWidth/2,mHeight/2,mWidth/2 + mRadius, mHeight/2 + mRadius, mCutLinesPaint);
+        canvas.drawLine(mWidth/2,mHeight/2,mWidth/2 + mRadius, mHeight/2 - mRadius, mCutLinesPaint);
+        canvas.drawLine(mWidth/2,mHeight/2,mWidth/2 - mRadius, mHeight/2 + mRadius, mCutLinesPaint);
     }
 }
